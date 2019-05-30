6ae304b63820f884a3335d57bce3627823d059dc
chips <chips@metrica.se>
2019-05-30 23:16:37 +0200

    Input component

---
+++ src/core/Input/Input.js
@@ -0,0 +1,36 @@
+import { getComponentProps } from '../../utils'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+export const Input = ({ tag, ...props }) => {
+  const Tag = tag
+  return <Tag {...props} />
+}
+
+Input.propTypes = {
+  className: PropTypes.string.isRequired,
+}
+
+Input.defaultProps = {
+  tag: 'input',
+}
+
+export const StyledInput = styled(
+  ({ theme, background, padding, ...props }) => <Input {...props} />
+)`
+  appearance: none;
+  border: 2px solid papayawhip;
+  background: ${props => props.theme.colors[props.background]};
+  padding: ${props => props.theme.spacings[props.padding]};
+`
+
+StyledInput.propTypes = {
+  ...Input.propTypes,
+}
+
+StyledInput.defaultProps = {
+  background: 'primary',
+  padding: 'md',
+}
+
+export default StyledInput
